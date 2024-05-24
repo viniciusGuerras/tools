717d6b177b9437126c82f28a1536ebf7e6550735
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-05-24 11:08:03 -0300

    second iteration, trying to do softmax-cross-entropy

--- digitRecognition.py
+++ digitRecognition.py
@@ -1,6 +1,141 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import numpy as np 
+import matplotlib.pyplot as plt
+
 
 from sklearn import datasets
-
-# Load the digits dataset
 digits = datasets.load_digits()
+
+X = digits.data
+y = digits.target
+
+class Dense_layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons));
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+class Activation_Relu:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)  
+        
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+
+class Leaky_Activation_Relu:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.where(inputs > 0, inputs, 0.01 * inputs)
+        
+    def backward(self, dvalues):
+        self.dinputs = np.where(self.inputs > 0, dvalues, 0.01 * dvalues)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        self.exponents = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.output = self.exponents / np.sum(self.exponents, axis=1, keepdims=True)
+    
+    def backward(self, dvalues):
+        dvalues = np.array(dvalues)
+        dvalues.sort(kind='quicksort' ,axis=0)[::-1]
+        dvalues[0] =  dvalues[0] * (1 - dvalues[0])
+        dvalues[0, :] *= dvalues[0]
+        self.dinputs = dvalues
+
+class Sum_Of_Squared_Residuals:
+    def forward(self, y_true, y_pred):
+        self.y_true = y_true
+        self.y_pred = y_pred
+        y_true = y_true.flatten()
+        y_pred = y_pred.flatten()
+        self.output = 0.5 * np.sum(((y_true - y_pred) ** 2))
+        return self.output
+    
+    def backward(self):
+        self.dinputs = self.y_pred - self.y_true
+        return self.dinputs
+
+class Cross_Entropy:
+    def forward(self, y_true, y_pred):
+        self.epsilon = 1e-15
+        self.y_pred = y_pred.flatten()
+        self.sample_size = y_true.shape[0]
+        self.output = -np.log(y_pred + self.epsilon)
+    def backward(self): 
+        self.output = (-1/(self.y_pred + self.epsilon))
+    
+class Optimizer_SGD:
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+    def update_params(self, layer):
+        layer.weights -= (self.learning_rate * layer.dweights)
+        layer.biases -= (self.learning_rate * layer.dbiases)
+
+
+class Model:
+    def __init__(self):
+        self.layers = []
+        self.activations = []
+
+    def addLayer(self, layer, activation=None):
+        self.layers.append(layer)
+        if activation is None:
+            self.activations.append(None)
+        else:
+            self.activations.append(activation)
+
+    def addLoss(self, loss):
+        self.lossFunction = loss
+
+    def addOptimizer(self, optimizer):
+        self.optimizer = optimizer
+
+    def forward(self, X):
+        self.core = X
+        for i in range(len(self.layers)):
+            self.layers[i].forward(self.core)
+            self.core = self.layers[i].output
+            if self.activations[i] is not None:
+                self.activations[i].forward(self.core)
+                self.core = self.activations[i].output
+
+    def backward(self, y_true):
+        loss_gradient = self.lossFunction.backward()
+        for i in range(len(self.layers) - 1, -1, -1):
+            if self.activations[i] is not None:
+                self.activations[i].backward(loss_gradient)
+                loss_gradient = self.activations[i].dinputs
+            self.layers[i].backward(loss_gradient)
+            loss_gradient = self.layers[i].dinputs
+    
+    def train(self, epochs, X, y):
+        for epoch in range(epochs):
+            self.forward(X)
+            loss = self.lossFunction.forward(y, self.core)
+
+            self.backward(y)
+            for layer in self.layers:
+                self.optimizer.update_params(layer)
+            if epoch % 100 == 0:
+                print(f"Epoch {epoch}: Loss = {loss}")
+            
+"""
+
+model = Model()
+model.addLayer(Dense_layer(64, 512), Activation_Relu())
+model.addLayer(Dense_layer(512, 512), Activation_Relu())
+model.addLayer(Dense_layer(512, 10), Activation_Softmax())
+model.addLoss(Cross_Entropy())
+model.addOptimizer(Optimizer_SGD(0.001))
+model.train(1000, X, y)"""
