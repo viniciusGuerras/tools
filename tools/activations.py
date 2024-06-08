8b229405c90945bc133fab434aca09868277dde7
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-06-08 19:32:53 -0300

    refactor: refactor:

--- tools/activations.py
+++ tools/activations.py
@@ -19,9 +19,9 @@ class Softmax:
         return self.output
 
     def backward(self, dvalues):
-        for index, (single_output, single_dvalues) in enumerate(zip(self.output,         dvalues)):
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output,dvalues)):
                 single_output = single_output.reshape(-1, 1)
-                jacobian_matrix = np.diagflat(single_output) - np.dot(single_output,             single_output.T)
+                jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
                 self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
                 
 class Tanh:
