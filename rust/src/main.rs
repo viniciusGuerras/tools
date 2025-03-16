f667dbee4e6be2cd7b634cd81a3d038aa191698a
Vinicius Guerra <itsviniciusguerra@gmail.com>
2025-03-16 17:42:52 -0300

    update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,28 +1,16 @@
 //rustup doc --book
 
-use regex::Regex;
 use std::{collections::{HashMap, HashSet}, vec};
 
-fn string_to_chars(input: Vec<&str>) -> Vec<Vec<String>> {
-    let re = Regex::new(r"[A-Za-z0-9]+|[[:punct:]]+").unwrap();
-    
-    let result: Vec<Vec<String>> = input
-        .iter()
-        .flat_map(|phrase| {
-            re.find_iter(phrase)  
-                .map(|mat| mat.as_str())  
-                .map(|token| {
-                    token.chars() 
-                        .map(|c| c.to_string())  
-                        .collect::<Vec<String>>()  
-                })
-        })
-        .collect();  
-
-    result
+fn string_to_u32(input: Vec<&str>) -> Vec<Vec<u32>> {
+    input.iter()
+            .map(|word| word.chars()
+                .map(|letter| letter as u32)
+                .collect())
+            .collect()
 }
 
-fn initialize_vocabulary(input: &Vec<Vec<String>>) -> Vec<String> {
+fn initialize_vocabulary(input: &Vec<Vec<u32>>) -> Vec<u32> {
     let mut unique_chars = HashSet::new();
     let mut result = Vec::new();
 
@@ -36,24 +24,21 @@ fn initialize_vocabulary(input: &Vec<Vec<String>>) -> Vec<String> {
     result
 }
 
-fn char_pair_frequency(phrases: &Vec<Vec<String>>) -> HashMap<Vec<char>, i32> {
-    let mut result: HashMap<Vec<char>, i32> = HashMap::new();
+fn byte_pair_frequency(phrases: &Vec<Vec<u32>>) -> HashMap<Vec<u32>, i32> {
+    let mut result: HashMap<Vec<u32>, i32> = HashMap::new();
 
     for word in phrases {
-        for n in 0..(word.len() - 1) {
-            let vector = [word[n].chars().collect::<Vec<char>>(), word[n + 1].chars().collect::<Vec<char>>()]
-                .concat();
-            
-            let counter = result.entry(vector).or_insert(0);
-            *counter += 1;
+        for n in 0..(word.len().saturating_sub(1)) {
+            let pair = vec![word[n], word[n + 1]]; 
+            *result.entry(pair).or_insert(0) += 1; 
         }
     }
 
     result
 }
 
-fn char_pair_leader(vector: &HashMap<Vec<char>, i32>) -> (Vec<char>, i32) {
-    let mut result: (Vec<char>, i32) = (Vec::new(), i32::MIN);
+fn byte_pair_biggest(vector: &HashMap<Vec<u32>, i32>) -> (u32, u32) {
+    let mut result: (Vec<u32>, i32) = (Vec::new(), i32::MIN);
 
     for (key, &value) in vector.iter() {
         if value > result.1 {
@@ -61,44 +46,69 @@ fn char_pair_leader(vector: &HashMap<Vec<char>, i32>) -> (Vec<char>, i32) {
         }
     }
 
-    result } 
+    (result.0[0], result.0[1])
+} 
 
-fn merge_chars(input: Vec<char>) -> String{
-     input.iter().collect() 
+fn merge(ids: &mut Vec<Vec<u32>>, pair: &(u32, u32), idx: u32){
+    for id_array in ids.iter_mut(){
+        let mut i = 0;
+        while i < (id_array.len() - 1) {
+            if id_array[i] == pair.0 && id_array[i + 1] == pair.1{
+                id_array.remove(i);
+                id_array[i] = idx;
+            }
+            i+=1;
+        }
+
+    }
 }
 
-fn update_vocabulary(training_chars: &Vec<Vec<String>>, vocabulary: &mut Vec<String>){
-    let result = char_pair_frequency(&training_chars);
-    let finished = char_pair_leader(&result);
-    vocabulary.push(merge_chars(finished.0));
+fn encoder(ids: &mut Vec<Vec<u32>>, vocab_size: u32) -> HashMap<(u32, u32), u32>{
+    let mut merges: HashMap<(u32, u32), u32> = HashMap::new();
+    let num_merges = vocab_size - 256; 
+    for i in 1..num_merges{
+        let frequency = byte_pair_frequency(&ids);
+        let pair = byte_pair_biggest(&frequency);
+        let new_id = 256 + i;
+        merge(ids, &pair, new_id);
+        merges.insert(pair, new_id);
+    }
+    merges
 }
 
-fn update_phrases(training_chars: &mut Vec<Vec<String>>, vocabulary: &mut Vec<String>) {
-    if vocabulary.is_empty(){
+fn recursive_decompose(merges: &HashMap<(u32, u32), u32>, list_result: &mut Vec<u32>, id: u32) {
+    if id < 256 {
+        list_result.push(id);
         return;
     }
 
-    let last_character = vocabulary.last().unwrap();
-    for word in training_chars.iter_mut(){
-        let mut s = 0;
-        while s < (word.len() - 1){
-            let combined = word[s].clone() + &word[s + 1];
-            if combined == *last_character{
-                word.remove(s);
-                word[s] = last_character.clone();
-            }
-            else{
-                s+=1;
-            }
-        }
+    if let Some((&(a, b), _)) = merges.iter().find(|&(_, &v)| v == id) {
+        recursive_decompose(merges, list_result, a);
+        recursive_decompose(merges, list_result, b);
     }
 }
 
-fn update(training_chars: &mut Vec<Vec<String>>, vocabulary: &mut Vec<String>, size: usize) {
-    (0..size).for_each(|_| {
-        update_vocabulary(training_chars, vocabulary);
-        update_phrases(training_chars, vocabulary);
-    });
+fn decoder(ids: Vec<Vec<u32>>, merges: HashMap<(u32, u32), u32>) -> Vec<Vec<String>> {
+    let mut result: Vec<Vec<String>> = Vec::new();
+
+    for id_line in ids {
+        let mut decoded_chars: String = String::new();
+
+        for &id in id_line.iter() {
+            let mut decoded_byte: Vec<u32> = Vec::new();
+            recursive_decompose(&merges, &mut decoded_byte, id);
+
+            for &byte in decoded_byte.iter() {
+                if let Some(character) = char::from_u32(byte) {
+                    decoded_chars.push(character);
+                }
+            }
+        }
+
+        result.push(vec![decoded_chars]);
+    }
+
+    result
 }
 
 
@@ -137,7 +147,10 @@ fn main(){
     ];
 
 
-    //i need to remove the whitespaces.
-
-    let mut reference_phrases = string_to_chars(training_data);
+    let mut reference_phrases = string_to_u32(training_data);
+    
+    let test = encoder(&mut reference_phrases, 280);
+    println!("{:?}", reference_phrases);
+    let result = decoder(reference_phrases, test);
+    println!("{:?}", result);
 }
